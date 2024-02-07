49271da29199f4e4f6db016327602cdff5f2a1ca
S <prrgrrmrr@gmail.com>
2024-02-07 17:47:46 +0100

    Implement prespective projection matrix

--- src/math/linear.c
+++ src/math/linear.c
@@ -1,9 +1,53 @@
 #include "linear.h"
 
-void Grr_identity(GrrMatrix4x4 *matrix) {
+void Grr_normalize3(GrrVector3 *v) {
+  Grr_f32 d = sqrtf(v->x * v->x + v->y * v->y + v->z * v->z);
+  v->x /= d;
+  v->y /= d;
+  v->z /= d;
+}
+
+void Grr_scale3(GrrVector3 *v, Grr_f32 s) {
+  v->x *= s;
+  v->y *= s;
+  v->y *= s;
+}
+
+void Grr_setLength3(GrrVector3 *v, Grr_f32 length) {
+  Grr_normalize3(v);
+  Grr_scale3(v, length);
+}
+
+void Grr_crossProduct(GrrVector3 *v, GrrVector3 *w, GrrVector3 *c) {
+  c->x = v->y * w->z - v->z * w->y;
+  c->y = v->z * w->x - v->x * w->z;
+  c->z = v->x * w->y - v->y * w->x;
+}
+
+void Grr_identityMatrix(GrrMatrix4x4 *matrix) {
   memset(matrix->data, 0, sizeof(matrix->data));
   matrix->data[0] = 1.0f;
   matrix->data[5] = 1.0f;
   matrix->data[10] = 1.0f;
   matrix->data[15] = 1.0f;
+}
+
+// Matrix to transform from local space (mesh) to world space
+// void Grr_modelMatrix(GrrMesh *mesh, GrrMatrix4x4 *matrix) {}
+
+// Transform from world space to camera's view space
+// void Grr_viewMatrix(GrrCamera *camera, GrrMatrix4x4 *matrix) {}
+
+// Projects vertices to clip space and performs perspective divide
+// Clip space uses left-to-right x-axis, downwards y-axis and the z-axis goes
+// from near clipping plane to far clipping plane
+void Grr_perspectiveProjectionMatrix(GrrCamera *camera, GrrMatrix4x4 *matrix) {
+  memset(matrix->data, 0, sizeof(matrix->data));
+  Grr_f32 t = tanf(camera->yFOV * 0.5);
+  matrix->data[0] = -1.0 / (camera->aspectRatio * t);
+  matrix->data[5] = -1.0 / t;
+  matrix->data[10] = -camera->zFar / (camera->zNear - camera->zFar);
+  matrix->data[11] = 1.0;
+  matrix->data[14] =
+      (-camera->zNear * camera->zFar) / (camera->zNear - camera->zFar);
 }
--- src/math/linear.h
+++ src/math/linear.h
@@ -3,15 +3,52 @@
 
 #include "string.h" // memcpy
 #include "types.h"
+#include <math.h>
 
 // GLSL types as defined in the spec:
 // https://registry.khronos.org/OpenGL/specs/gl/GLSLangSpec.4.60.pdf
+
+typedef struct GrrVector2 {
+  Grr_f32 x;
+  Grr_f32 y;
+} GrrVector2;
+
+typedef struct GrrVector3 {
+  Grr_f32 x;
+  Grr_f32 y;
+  Grr_f32 z;
+} GrrVector3;
+
+typedef struct GrrVector4 {
+  Grr_f32 x;
+  Grr_f32 y;
+  Grr_f32 z;
+  Grr_f32 w;
+} GrrVector4;
 
 typedef struct GrrMatrix4x4 {
   Grr_f32
       data[16]; // Column-major order: first 4 floats represent first column...
 } GrrMatrix4x4;
 
-void Grr_identity(GrrMatrix4x4 *matrix);
+//  Camera uses right-handed coordinate system (facing the negative z axis)
+typedef struct GrrCamera {
+  GrrVector3 origin;    // Camera location
+  GrrVector3 direction; // The -z axis of the camera coordinate system
+  GrrVector3 upHint;    // Camera y-axis hint: any vector with positive camera y
+                        // component, this will be used to calculate the true
+                        // camera y-axis
+  Grr_f32 zNear;        // Positive distance (in meters) to near clipping plane
+  Grr_f32 zFar;         // Positive distance (in meters) to far clipping plane
+  Grr_f32 aspectRatio;  // width:height ratio
+  Grr_f32 yFOV;         // Vertical field of view in radians (< 3.14159...)
+} GrrCamera;
+
+void Grr_normalize3(GrrVector3 *v);
+void Grr_scale3(GrrVector3 *v, Grr_f32 s);
+void Grr_setLength3(GrrVector3 *v, Grr_f32 length);
+void Grr_crossProduct(GrrVector3 *v, GrrVector3 *w, GrrVector3 *c);
+void Grr_identityMatrix(GrrMatrix4x4 *matrix);
+void Grr_perspectiveProjectionMatrix(GrrCamera *camera, GrrMatrix4x4 *matrix);
 
 #endif
